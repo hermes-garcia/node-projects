d89a50e98bd1a04c4101e888c89f33d0e956b6a6
Hermes <hermes.garcia@bounteous.com>
2023-06-28 20:03:15 -0600

    first-commit

---
+++ 01-hello-world/app.js
@@ -0,0 +1,9 @@
+//console.log('Hello world!');
+
+console.log('Start 1');
+
+setTimeout(() => console.log('2'),3000);
+setTimeout(() => console.log('3'),0);
+setTimeout(() => console.log('4'),0);
+
+console.log('End! 5');
